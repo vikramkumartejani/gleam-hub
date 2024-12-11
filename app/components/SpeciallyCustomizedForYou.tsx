65bdd24482dbc813e75cc3a165c28018206aac7b
vikram kumar <vikramkumar8t8@gmail.com>
2024-12-12 02:28:03 +0500

    update

--- app/components/SpeciallyCustomizedForYou.tsx
+++ app/components/SpeciallyCustomizedForYou.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SpeciallyCustomizedForYou = () => {
   return (
-    <div className='py-[107px] w-full relative'>
+    <div className='pt-[107px] pb-[85px] w-full relative'>
         <Image src='/assets/Vector 40.svg' alt='Vector' width={110} height={90} className=' absolute top-12 left-0' />
         <div className='relative z-30 text-center'>
             <h4 className='text-[#A67C52] text-[15px] leading-[18px] font-inter font-semibold tracking-[0.05em] mb-2'>OUR SERVICES</h4>
@@ -39,6 +39,48 @@ const SpeciallyCustomizedForYou = () => {
                     <div className='relative'>
                         <Image src='/assets/crown-confidence.svg' alt='scalp-revival' width={511} height={374} className='rounded-[87px] relative z-20' />
                         <Image src='/assets/Vector 41.svg' alt='Vector' width={244} height={269} className=' absolute top-0 -right-20' />
+                    </div>
+                </div>
+            </div>
+               
+            <div className='pt-[51px] pb-[51px] max-w-[1168px] mx-auto w-full flex items-center gap-3 justify-between'>
+                <Image src='/assets/restore-thrive.svg' alt='restore-thrive' width={511} height={374} className='rounded-[87px]' />
+                <div className='text-left max-w-[606px]'>
+                    <h2 className='text-[#282828] text-[48px] leading-[73px] font-inter font-extrabold tracking-[-0.02em]'>Restore & Thrive</h2>
+                    <p className='text-[#282828] text-[16px] font-normal font-poppins leading-[26px]'>For women struggling with thinning hair or hair loss, this programme offers expert guidance for natural hair care and proven methods to regain fullness and strength.</p>
+                    <ul className='ml-6 text-[#282828] text-[16px] font-normal font-poppins leading-[26px]'>
+                        <li className='list-disc'>Thorough analysis to identify the root causes of hair loss.</li>
+                        <li className='list-disc'>Tailoured solutions for restoration and regrowth.</li>
+                        <li className='list-disc'>Continued support to ensure progress and success.</li>
+                    </ul>
+                    <h4 className='text-[#A67C52] text-[20px] leading-[45px] font-inter font-extrabold uppercase'>R700 first visit, R350 per follow-up visit</h4>
+                </div>
+            </div>
+
+            <button className='mt-7 bg-[#B2875C] border-[0.7px] border-[#FEFEFE3B] w-[272px] h-[80px] uppercase rounded-[40px] font-inter text-[#FFEFDE] text-[17px] font-medium tracking-[-0.03em]'>BOOK CONSULTATION</button>
+
+            <div className='mt-20 bg-[#A67C52] relative py-[66px] w-full px-4 max-w-[1240px] mx-auto rounded-2xl' style={{boxShadow: "0px 19px 36.2px 7px #0000000A"}}>
+                <img src='/assets/pinterest.svg' alt='pinterest' className='w-full absolute bg-bottom h-full inset-0' />
+                <div className="max-w-[988px] mx-auto w-full">
+                    <div className='flex items-center justify-between gap-2 mb-8'>
+                        <h1 className='text-[#FFFFFF] text-[56px] leading-[65px] font-inter font-bold tracking-[-2px] max-w-[496px] text-left'>What Sets Gleam Hub Apart?</h1>
+                        <button className='bg-[#FDF5EE] border-[0.7px] border-[#FEFEFE3B] w-[213px] h-[60px] rounded-[59px] font-inter text-[#A67C52] text-[18px] font-bold tracking-[-0.03em]'>Book Appointment</button>
+                    </div>
+                    <div className='mt-[62px] flex items-center text-left gap-10'>
+                        <div className='max-w-[273px]'>
+                            <h2 className='mb-3 text-[#FFFFFF] text-[20px] leading-[28px] font-bold font-manrope'>Expertise You Can Trust</h2>
+                            <p className='text-[#FFFFFFE3] text-[14px] font-normal font-manrope leading-[25px]'>You will be tended to by a certified professional who is trained in textured hair restoration, ensuring you receive care.</p>
+                        </div>
+                        <div className='bg-[#FFFFFF45] w-[1px] h-[59px]'></div>
+                        <div className='max-w-[273px]'>
+                            <h2 className='mb-3 text-[#FFFFFF] text-[20px] leading-[28px] font-bold font-manrope'>Expertise You Can Trust </h2>
+                            <p className='text-[#FFFFFFE3] text-[14px] font-normal font-manrope leading-[25px]'>You will be tended to by a certified professional who is trained in textured hair restoration, ensuring you receive care.</p>
+                        </div>
+                        <div className='bg-[#FFFFFF45] w-[1px] h-[59px]'></div>
+                        <div className='max-w-[273px]'>
+                            <h2 className='mb-3 text-[#FFFFFF] text-[20px] leading-[28px] font-bold font-manrope'>Expertise You Can Trust </h2>
+                            <p className='text-[#FFFFFFE3] text-[14px] font-normal font-manrope leading-[25px]'>You will be tended to by a certified professional who is trained in textured hair restoration, ensuring you receive care.</p>
+                        </div>
                     </div>
                 </div>
             </div>
